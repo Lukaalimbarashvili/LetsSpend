bcc4ddf4d21283109bfd04f6342d4f340d49ef3e
Lukaalimbarashvili <aalimbara@gmail.com>
2021-04-02 01:43:59 +0400

    Update ViewController.swift

--- LetsSpend/ViewControllers/Main Screen/ViewController.swift
+++ LetsSpend/ViewControllers/Main Screen/ViewController.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("fefe")
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemCell",for: indexPath) as! ItemCell
+//        cell.configure(movie: UIImage(named: "car")!)
+//        cell.price.text = "300 000$"
+        return cell
+    }
+    
+}
